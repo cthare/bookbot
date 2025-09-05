ae4924bd29bf8fb80c40e8f0dd2a2d6ce703b671
cthare <colin@colinhare.com>
2025-09-04 20:03:23 -0400

    Removed example book hard codings - Added arguments for book path

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+import sys
 from stats import count_words, count_characters
 
-path_to_file = "books/frankenstein.txt"
-
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path_to_file = sys.argv[1]
+
     with open(path_to_file) as f:
         file_contents = f.read()
 
     word_report = count_words(file_contents)
     character_report = count_characters(file_contents)
 
-    print("--- Begin report of books/frankenstein.txt ---")
+    print("============ BOOKBOT ============")
+    print(f"--- Begin report of {path_to_file} ---")
+    print("----------- Word Count ----------")
     print(f"{word_report} words found in the document")
-    print("")
+    print("--------- Character Count -------")
     for i in character_report:
-        print(f"The '{i} character was found {character_report[i]} times")
+        print(f"{i}: {character_report[i]}")
 
-    print("--- End report ---")
+    print("============= END ===============")
 
 main()
